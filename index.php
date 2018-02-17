d4162a7f89ad5106d76b29d431353ad8d8e7c8a4
Alaa Aqeel <alaa.21.iraq@gmail.com>
2018-02-17 02:34:08 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php 
+
+
+include_once 'include/setting.php';
+
+
+$url = explode("/", $_SERVER["REQUEST_URI"]);
+$path = $url[2] == "" ? "MPage": $url[2];
+
+if ( sizeof($url) > 3 ){
+    header("Location:/".$url[1]."/".$path);
+}
+
+$url = explode("?",$path);
+
+if (sizeof($url) == 2){
+
+    $path = $url[0];
+    
+}
+
+
+
+include_once "Pages/layout/header.php";
+
+
+
+if (file_exists("Pages/".$path.".php")){
+
+    include_once("Pages/".$path.".php");
+
+}else{ 
+
+    include_once("Pages/MPage.php");
+
+}
+
+
+include_once "Pages/layout/foter.php";
+
+
+?>
