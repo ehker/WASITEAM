7afdaa79eec080cef4d1ecf0c111604aafc180d0
Alaa Aqeel <alaa.21.iraq@gmail.com>
2018-02-17 02:37:39 -0800

    Add files via upload

---
+++ Pages/Teams.php
@@ -0,0 +1,77 @@
+
+
+<div class="col-xl-3 col-lg-3 col-md-3 col-sm-4 project member-1 wow animated animated3 fadeInLeft text-right" dir="rtl">
+    <div class="project-hover">
+        <h2>طه ياسين</h2>
+        <hr />
+        <p>محب للتقنية ، يعمل كمحرر لصحيفة المساء</p>
+        <a href="profile?id=1">الملف الشخصي</a>
+    </div>
+</div>
+<div class="col-xl-3 col-lg-3 col-md-3 col-sm-4 project member-2 wow animated animated3 fadeInLeft text-right" dir="rtl">
+    <div class="project-hover">
+        <h2>حيدر حاتم</h2>
+        <hr />
+        <p>محب للتقنية ، يعمل كمحرر لصحيفة المساء</p>
+        <a href="profile?id=2">الملف الشخصي</a>
+    </div>
+</div>
+<div class="col-xl-3 col-lg-3 col-md-3 col-sm-4 project member-3 wow animated animated3 fadeInLeft text-right" dir="rtl">
+    <div class="project-hover">
+        <h2>امال حسين</h2>
+        <hr />
+        <p>محب للتقنية ، يعمل كمحرر لصحيفة المساء</p>
+        <a href="profile?id=3">الملف الشخصي</a>
+    </div>
+</div>
+<div class="col-xl-3 col-lg-3 col-md-3 col-sm-4 project member-4 wow animated animated3 fadeInLeft text-right" dir="rtl">
+    <div class="project-hover">
+        <h2>يحيى محمد</h2>
+        <hr />
+        <p>محب للتقنية ، يعمل كمحرر لصحيفة المساء</p>
+        <a href="profile?id=4">الملف الشخصي</a>
+    </div>
+</div>
+<div class="col-xl-3 col-lg-3 col-md-3 col-sm-4 project member-5 wow animated animated3 fadeInLeft text-right" dir="rtl">
+    <div class="project-hover">
+        <h2>امير امجد</h2>
+        <hr />
+        <p>محب للتقنية ، يعمل كمحرر لصحيفة المساء</p>
+        <a href="profile?id=5">الملف الشخصي</a>
+    </div>
+</div>
+<div class="col-xl-3 col-lg-3 col-md-3 col-sm-4 project member-6 wow animated animated3 fadeInLeft text-right" dir="rtl">
+    <div class="project-hover">
+        <h2>منتظر نعيم</h2>
+        <hr />
+        <p>محب للتقنية ، يعمل كمحرر لصحيفة المساء</p>
+        <a href="profile?id=6">الملف الشخصي</a>
+    </div>
+</div>
+<div class="col-xl-3 col-lg-3 col-md-3 col-sm-4 project member-7 wow animated animated3 fadeInLeft text-right" dir="rtl">
+    <div class="project-hover">
+        <h2>حنين حسن</h2>
+        <hr />
+        <p>محب للتقنية ، يعمل كمحرر لصحيفة المساء</p>
+        <a href="profile?id=7">الملف الشخصي</a>
+    </div>
+</div>
+<div class="col-xl-3 col-lg-3 col-md-3 col-sm-4 project member-8 wow animated animated3 fadeInLeft text-right" dir="rtl">
+    <div class="project-hover">
+        <h2>نائل فاضل</h2>
+        <hr />
+        <p>محب للتقنية ، يعمل كمحرر لصحيفة المساء</p>
+        <a href="profile?id=8">الملف الشخصي</a>
+    </div>
+</div>
+
+
+
+<div class="col-xl-3 col-lg-3 col-md-3 col-sm-4 project member-9 wow animated animated3 fadeInLeft text-right" dir="rtl">
+    <div class="project-hover">
+        <h2>Alaa Aqeel</h2>
+        <hr />
+        <p>Programmer And Developer</p>
+        <a href="profile?id=9">الملف الشخصي</a>
+    </div>
+</div>
